76545810aed05d769274daee777c102e5aa5eeb3
Vladimir Leshkevich <v.leshkevich@tcp-soft.com>
2019-09-17 15:54:17 +0300

    commets deleted

--- hasher.cpp
+++ hasher.cpp
@@ -10,7 +10,5 @@ void hasher::operator()(size_t i, const std::vector<std::string>& data,
     std::cout << "Thread " << i << " use data " << data[i] << std::endl;
     hash_result = md5(data[i]);
     std::cout << "hash result vector =  " << hash_result << std::endl;
-    // std::lock_guard<std::mutex> locked(report_mutex);
-    // result.push_back(hash_result);
     result[i] = hash_result;
 }
